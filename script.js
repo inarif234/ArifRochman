1b3f1f7c57333b085e325e454e0ecadf32280a7f
Arif Rochman <clippy789@gmail.com>
2026-04-02 23:04:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ let currentIndex = 0;
 let images = [];
 
 /* ================= DATA GAMBAR ================= */
-/* SUSUNAN FOLDER: img/web/, img/sosmed/, img/logo/, img/print/ */
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-const data = {
+/* ================= DATA DESKTOP ================= */
+const dataDesktop = {
   0: [
     "img/web/web1.jpg",
     "img/web/web2.jpg",
@@ -30,23 +31,60 @@ const data = {
     "img/sosmed/sosmed6.jpg",
     "img/sosmed/sosmed7.jpg",
     "img/sosmed/sosmed8.jpg",
-    "img/sosmed/sosmed9.jpg"
+    "img/sosmed/sosmed9.jpg",
   ],
 
   2: [
     "img/logo/logo1.jpg",
     "img/logo/logo2.jpg",
     "img/logo/logo3.jpg",
-    "img/logo/logo4.jpg"
+    "img/logo/logo4.jpg",
   ],
 
   3: [
     "img/print/print1.jpg",
     "img/print/print2.jpg",
-    "img/print/print3.jpg"
+    "img/print/print3.jpg",
   ]
 };
 
+/* ================= DATA MOBILE ================= */
+const dataMobile = {
+  0: [
+    "img/web-mobile/web1.jpg",
+    "img/web-mobile/web2.jpg",
+    "img/web-mobile/web3.jpg",
+    "img/web-mobile/web4.jpg",
+  ],
+
+  1: [
+    "img/sosmed-mobile/sosmed1.jpg",
+    "img/sosmed-mobile/sosmed2.jpg",
+    "img/sosmed-mobile/sosmed3.jpg",
+    "img/sosmed-mobile/sosmed4.jpg",
+    "img/sosmed-mobile/sosmed5.jpg",
+    "img/sosmed-mobile/sosmed6.jpg",
+    "img/sosmed-mobile/sosmed7.jpg",
+    "img/sosmed-mobile/sosmed8.jpg",
+    "img/sosmed-mobile/sosmed9.jpg",
+  ],
+
+  2: [
+    "img/logo-mobile/logo1.jpg",
+    "img/logo-mobile/logo2.jpg",
+    "img/logo-mobile/logo3.jpg",
+    "img/logo-mobile/logo4.jpg",
+  ],
+
+  3: [
+    "img/print-mobile/print1.jpg",
+    "img/print-mobile/print2.jpg",
+    "img/print-mobile/print3.jpg",
+  ]
+};
+
+/* ================= FINAL DATA ================= */
+const data = isMobile ? dataMobile : dataDesktop;
 /* ================= CLICK CARD ================= */
 cards.forEach((card, i) => {
   card.addEventListener("click", () => {
